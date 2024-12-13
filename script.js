c5ffd18a837e0447d70cb6ddfcc491b6c447459a
Luciene <lucienedelima8@gmail.com>
2024-12-13 18:18:40 -0300

    fiz a parte atualizando os dados do cartão

--- script.js
+++ script.js
@@ -23,7 +23,28 @@ function atualizar_cartao() {
     /*CVC*/
     const cvc = document.getElementById("cvc").value;
 
+
+
+
+
+    /*Atualizando os dados do cartão*/
+
+
+    document.getElementById("id-nome_cartao").innerHTML = nome || "Nome Titular";
+
+    document.getElementById("id-numero_cartao").innerHTML = numero_cartao || "XXXX XXXX XXXX XXXX";
+
+    document.getElementById("id-expira_cartao").innerHTML = mes && ano ? `${mes}/${ano}` : "MM/YY";
+
+    document.getElementById("id-cvc_cartao").innerHTML = cvc || "CVC";
+
+
+
+
+
+
     
+
 
 
 
