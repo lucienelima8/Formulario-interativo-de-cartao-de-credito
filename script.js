c45e6a208b0533090fca79a4162490d7193026a6
Luciene <lucienedelima8@gmail.com>
2024-12-13 16:23:35 -0300

    iniciando o JS

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/*Função pra inserir os dados no cartão*/
+function atualizar_cartao() {
+
+    /*Pegando os dados dos inputs*/
+
+
+    /*Frente do cartão*/
+    /*Nome*/
+    const nome = document.getElementById("nome_titular").value;
+
+    /*Número do cartão*/
+    const numero_cartao = document.getElementById("numero_cartao").value;
+
+    /*Mês*/
+    const mes = document.getElementById("mes").value;
+
+    /*Ano*/
+    const ano = document.getElementById("ano").value;
+
+
+    /*Verso do cartão*/
+    /*CVC*/
+    const cvc = document.getElementById("cvc").value;
+
+    
+
+
+
+
+
+
+
+}
+
