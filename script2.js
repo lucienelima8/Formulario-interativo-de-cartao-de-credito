96286c78e79567604f11b16bbafb1c035279809b
Luciene <lucienedelima8@gmail.com>
2024-12-18 12:02:18 -0300

    fazendo um segundo script

---
+++ script2.js
@@ -0,0 +1,35 @@
+/*Exibição no cartão*/
+
+/*Número do display frente do cartão*/
+const num_display_card = document.getElementById("id-num_display_card");
+/*Nome do display frente do cartão*/
+const nome_display_card = document.getElementById("id-nome_display_card");
+/*Vencimento do cartão na frente do cartão*/
+const vencto_card = document.getElementById("id-vencto_card");
+/*CVC 3 números verso do cartão*/
+const cvc_display = document.getElementById("id-cvc_card_display");
+
+
+
+
+/*INSERÇÃO DE DADOS*/
+
+/*Cardholder Name (Nome do titular)*/
+/**const label_nome = document.getElementsByClassName("label_nome");
+
+/*input nome*/
+const input_nome_titular = document.getElementById("nome_titular");
+
+/*número*/
+const number = document.getElementById("numero_cartao");
+
+/*mês*/
+const mes = document.getElementById("mes");
+
+/*ano*/
+const ano = document.getElementById("ano");
+
+/*cvc*/
+const cvc = document.getElementById("cvc");
+
+
